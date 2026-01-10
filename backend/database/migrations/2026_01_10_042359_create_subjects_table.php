72100eb787f49755adc4363856ad3208f0b0b475
Breno Guimarães <devbreno4@gmail.com>
2026-01-10 01:50:33 -0300

    initial commit

---
+++ backend/database/migrations/2026_01_10_042359_create_subjects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('maximum_allowed_absence_percentage');
+            $table->integer('total_hours');
+            $table->integer('hours_per_class');
+            $table->string('color');
+            $table->foreignId('semester_id')->references('id')->on('semesters')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
